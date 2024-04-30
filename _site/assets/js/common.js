19d4f7a9294b7790f56498bd8e90c32d8518247e
XuanKhanh Nguyen <ngxuankhanhdl@gmail.com>
2024-04-30 16:04:49 -0400

    jekyll

---
+++ _site/assets/js/common.js
@@ -0,0 +1,31 @@
+// Function to fetch and insert HTML content into a specified element
+function fetchAndInsertHTML(url, elementId) {
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
+    // Read the response as text
+    .then(html => {
+      // Create a div element to contain the fetched HTML content
+      const container = document.createElement('div');
+      container.innerHTML = html;
+
+      // Insert the modified HTML content into the placeholder element
+      document.getElementById(elementId).appendChild(container);
+    })
+    .catch(error => {
+      console.error('There was a problem fetching the HTML:', error);
+    });
+}
+
+// Wrap the function calls in an event listener to ensure they run after the DOM is loaded
+document.addEventListener("DOMContentLoaded", function() {
+  // Call the fetchAndInsertHTML function to fetch and insert header.html into the header element
+  fetchAndInsertHTML('../components/header.html', 'header-placeholder');
+
+  // Call the fetchAndInsertHTML function to fetch and insert footer.html into the footer element
+  fetchAndInsertHTML('../components/footer.html', 'footer-placeholder');
+});
